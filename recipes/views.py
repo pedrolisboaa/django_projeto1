5375a567575e1547196e0e119de66d23708989d0
pedrolisboaa <phlisboa2000@gmail.com>
2022-06-06 22:50:54 -0300

    Dia de hoje!

--- recipes/views.py
+++ recipes/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Pedro Lisboa'
     })
 
